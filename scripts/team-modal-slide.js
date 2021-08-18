0fb8935f007ac254dc0a2a929b950f91bae19c3a
Ivan <lorf1991@gmail.com>
2021-08-19 00:02:48 +0300

    add change in section team

--- scripts/team-modal-slide.js
+++ scripts/team-modal-slide.js
@@ -1,9 +1,10 @@
-const modalTrigger = document.querySelectorAll("[data-modal-team]");
-const modalItem = document.querySelectorAll("[modal-team-item]");
-const modalClose = document.querySelectorAll("[modal-team-close]");
+const modalTrigger = document.querySelectorAll(".team__item");
+const modalItem = document.querySelectorAll(".team__modal");
+const modalClose = document.querySelectorAll(".team__modal-close");
 
 const removeBlock = () => {
   modalItem.forEach((item) => item.classList.add("d-none"));
+  document.body.style.overflow = "";
 };
 
 removeBlock();
@@ -11,7 +12,26 @@ removeBlock();
 for (let i = 0; i < modalTrigger.length; i += 1) {
   modalTrigger[i].addEventListener("click", () => {
     modalItem[i].classList.remove("d-none");
+    document.body.style.overflow = "hidden";
   });
 }
 
-modalClose.forEach((e) => e.addEventListener("click", removeBlock));
+modalClose.forEach((e) =>
+  e.addEventListener("click", () => {
+    removeBlock();
+  })
+);
+
+modalItem.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    if (e.target === item) {
+      removeBlock();
+    }
+  })
+);
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    removeBlock();
+  }
+});
