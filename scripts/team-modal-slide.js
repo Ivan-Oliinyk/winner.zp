79b6e3cb99a646d32c29867907c9bba4fa6f9467
Ivan <lorf1991@gmail.com>
2021-08-20 10:30:54 +0300

    fixed bag in mosdal-slider

--- scripts/team-modal-slide.js
+++ scripts/team-modal-slide.js
@@ -1,27 +1,59 @@
 const modalTrigger = document.querySelectorAll(".team__item");
 const modalItem = document.querySelectorAll(".team__modal");
 const modalClose = document.querySelectorAll(".team__modal-close");
+const prevArrow = document.querySelectorAll(".team__modal-arrow_left");
+const nextArrow = document.querySelectorAll(".team__modal-arrow_right");
 
 const removeBlock = () => {
   modalItem.forEach((item) => item.classList.add("d-none"));
   document.body.style.overflow = "";
 };
 
-// removeBlock();
+const showItem = (itemIndex) => {
+  modalItem[itemIndex].classList.remove("d-none");
+  document.body.style.overflow = "hidden";
+};
 
-for (let i = 0; i < modalTrigger.length; i += 1) {
-  modalTrigger[i].addEventListener("click", () => {
-    modalItem[i].classList.remove("d-none");
-    document.body.style.overflow = "hidden";
-  });
-}
+modalTrigger.forEach((item, i) =>
+  item.addEventListener("click", () => {
+    showItem(i);
+  })
+);
 
+//листаем модельные окна влево при клике на стрелку влево
+prevArrow.forEach((elem, index) =>
+  elem.addEventListener("click", () => {
+    removeBlock();
+
+    if (index > 0) {
+      showItem(index - 1);
+    } else {
+      showItem(0);
+    }
+  })
+);
+
+//листаем модельные окна вправо при клике на стрелку вправо
+nextArrow.forEach((elem, index, nextArrow) =>
+  elem.addEventListener("click", () => {
+    removeBlock();
+
+    if (index < nextArrow.length - 1) {
+      showItem(index + 1);
+    } else {
+      showItem(nextArrow.length - 1);
+    }
+  })
+);
+
+//закрываем модельное окно при клике на крест
 modalClose.forEach((e) =>
   e.addEventListener("click", () => {
     removeBlock();
   })
 );
 
+//закрываем модельное окно при клике вне его тела
 modalItem.forEach((item) =>
   item.addEventListener("click", (e) => {
     if (e.target === item) {
@@ -30,6 +62,7 @@ modalItem.forEach((item) =>
   })
 );
 
+//закрываем модельное окно при клике на клавишу ESC
 document.addEventListener("keydown", (e) => {
   if (e.code === "Escape") {
     removeBlock();
